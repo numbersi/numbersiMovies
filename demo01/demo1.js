a7b2ab8109683ddedd698f00762068426a99ac72
javaxx <136222866@qq.com>
2018-11-16 12:53:29 +0800

    ‘什么也没有’

---
+++ demo01/demo1.js
@@ -0,0 +1,28 @@
+const puppeteer =  require('puppeteer')
+const devices = require('puppeteer/DeviceDescriptors')
+const iPhone =devices['Nexus 7']
+let url = process.argv.splice(2)[0]
+const sleep = time => new Promise(resolve =>{
+    setTimeout(resolve,time)
+})
+url ='http://api.bbbbbb.me/jx/v.php?url='+url
+// url = 'https://www.baidu.com'
+
+puppeteer.launch()
+    .then(
+        async browser => {
+            const page = await browser.newPage()
+
+            let userAgent='Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
+            await page.setUserAgent(userAgent)
+            await page.goto(
+                url
+                ,{waitUntil:'networkidle2'}
+                );
+            const data = await page.$eval('video',v=> v.src)
+            console.log(data);
+            
+            browser.close()
+        }
+        
+    )
