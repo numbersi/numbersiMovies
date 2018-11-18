92d43bec06738c9b5af973d09b6a5998434c560e
numbersi <136222866@qq.com>
2018-11-18 18:21:25 +0800

    '增加了获取播放地址'

--- demo01/demo2.js
+++ demo01/demo2.js
@@ -1,45 +1,48 @@
 const puppeteer =  require('puppeteer')
 const devices = require('puppeteer/DeviceDescriptors')
 const iPhone =devices['Nexus 7']
-let url = process.argv.splice(2)[0]
+
+
 const sleep = time => new Promise(resolve =>{
     setTimeout(resolve,time)
 })
-// url ='http://api.bbbbbb.me/jx/v.php?url='+url
-// url ='https://api.lldyy.net/vip/?url=https://v.youku.com/v_show/id_XMzkxMTMzMjI2NA==.html'
-// url = 'https://www.baidu.com'
-url='http://app.baiyug.cn:2019/vip/index.php?url='+url
 
-puppeteer.launch()
-    .then(
-        async browser => {
-            const page = await browser.newPage()
 
-            let userAgent='Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
-            await page.setUserAgent(userAgent)
-            console.log("kaishi");
-            console.log(url);
+const getPlayUrl= async function(url){
+   var playUrl = null
+   await  puppeteer.launch()
+.then(
+    async browser => {
+        const page = await browser.newPage()
 
-            await page.goto(
-                url
-                ,{waitUntil:'networkidle2'}
-                );
-            const data = await page.$eval('body',()=>{})
-            console.log(data);
-           const a =  await page.frames()
-          
-           console.log( a.length);
+        let userAgent='Mozilla/5.0 (Linux; U; Android 4.3; en-us; SM-N900T Build/JSS15J) AppleWebKit/534.30 (KHTML, like Gecko) Version/4.0 Mobile Safari/534.30'
+        await page.setUserAgent(userAgent)
+        console.log("kaishi");
+        console.log(url);
 
-           if (a.length){
-            Object.keys(a).forEach(function(key){
-                console.log(key);
-                
-            })
-             console.log(await a[a.length-1].$eval('video',v=> v.src));
-           
-           }
-  
-           browser.close()
-        }
-        
-    )
+        await page.goto(
+            url
+            ,{waitUntil:'networkidle2'}
+            );
+        const data = await page.$eval('body',()=>{})
+        console.log(data);
+       const a =  await page.frames()
+      
+       console.log( a.length);
+
+       if (a.length){
+        Object.keys(a).forEach(function(key){
+            console.log(key);
+            
+        })
+        playUrl = await a[a.length-1].$eval('video',v=> v.src)
+       }
+
+    }
+)
+
+    return playUrl
+
+}
+
+module.exports =  {getPlayUrl}
--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,10 @@
 const router = require('koa-router')()
 var request = require('request-promise');
 var cheerio = require('cheerio');
+var {getPlayUrl} = require('../lib/lib-getPlayUrl');
+var {resolve} = require('path')
+const { exec } = require('child_process');
+
 // var Buffer = require('buffer')
 router.prefix('/search')
 
@@ -12,26 +16,49 @@ router.get('/movie/:kw', async (ctx, next) => {
 
 })
 
-router.get('/play/:tabs', async (ctx,next)=>{
+router.get('/play/:type/:tabs', async (ctx,next)=>{
     tabs=ctx.params.tabs
+    type=ctx.params.type
     tabs = decodeTab(tabs)
     let seriesNumbers ={}
-
+    console.log(seriesNumbers);
+    let playTemplet= 'dianshi-dianying'
     for(let i = 0; i　< tabs.length; i++) {
         tab = tabs[i]
-        tab =tab.split('_').splice(1)
-        const url = await getSeriesNumber(tab)
+        tab =tab.split('_').splice(1).sort((a,b)=>{})
+        let url =null 
+        switch (type) {
+            case "zongyi":
+                url= await getZongyiSeriesNumber(tab)
+                playTemplet= 'zongyi'
+                break;
+            case "dianying":
+                url= await getZongyiSeriesNumber(tab)
+                break;
+            case "dianshi":
+                url= await getDianshiSeriesNumber(tab)
+                break;
+            default:
+                break;
+        }
+        console.log('url',url);
+        
         seriesNumbers[tab[2]]=url
     }
-
-    // 这个方法  seriesNumbers 无效 
-    // await tabs.forEach(async (tab,k) => {
-    //    tab =tab.split('_').splice(1)
-    //    const url = await getSeriesNumber(tab)
-    //    seriesNumbers[tab[2]]=url
-    // });
+    console.log(seriesNumbers);
     
-    await ctx.render ('play',{tabs:Object.keys(seriesNumbers),seriesNumbers:seriesNumbers}) 
+    await ctx.render ('play',{tabs:Object.keys(seriesNumbers),seriesNumbers:seriesNumbers,playTemplet:playTemplet}) 
+})
+router.get('/getPlayUrl',async (ctx,next)=>{
+    url=ctx.query.url
+    const util = require('util');
+    const execFile = util.promisify(require('child_process').execFile);
+    const script = resolve(__dirname,'../crawler/playUrl.js')
+    const { stdout } = await execFile('node', [script,'http://app.baiyug.cn:2019/vip/index.php?url='+url]);
+    console.log(stdout);
+
+    ctx.body=stdout
+
 })
 
 // 加密 encodeUrl
@@ -41,7 +68,8 @@ function encodeTab(tabs){
 }
 // 解密
 function decodeTab(codeTab){
-    return new Buffer(codeTab, 'base64').toString().split('$')
+    let tabs =  new Buffer(codeTab, 'base64').toString().split('$')
+    return tabs.sort(function(a,b){return b.length - a.length;});
 }
 async function  searchDataFrom360kan(kw){
     // url = `https://so.360kan.com/index.php?kw=${kw}&from=`
@@ -51,34 +79,44 @@ async function  searchDataFrom360kan(kw){
     const html  =  await request(url)
     var $ = cheerio.load(html)
     var movieList = [];
-    $('.index-dianying').each(function(){
-        let $this = $(this);
-        movieList = aa($,$this,movieList)
+
+    ['zongyi','dianying','dianshi'].forEach((type)=>{
+        $('.index-'+type).each(function(){
+            let $this = $(this);
+            movieList = aa($,$this,movieList,type)
+        })
+    // $('.index-zongyi').each(function(){
+    //     let $this = $(this);
+    //     movieList = aa($,$this,movieList,)
+    // })
+    // $('.index-dianying').each(function(){
+    //     let $this = $(this);
+    //     movieList = aa($,$this,movieList)
        
-    })
-    $('.index-dianshi').each(function(){
-        let $this = $(this);
-        movieList = aa($,$this,movieList)
-       
+    // })
+    // $('.index-dianshi').each(function(){
+    //     let $this = $(this);
+    //     movieList = aa($,$this,movieList)
+    // })
     })
     return movieList
 }
-function aa($,dom,movieList){
+function aa($,dom,movieList,type){
     var $this = dom
     const moviePic  = $this.find('.m-mainpic a img').attr("src")
     const cont =$this.find('.cont')
     const titleDom = cont.find('.title')
     const title = titleDom.find('a b').text()
     const playtype =titleDom.find('.playtype').text()
-    const score = titleDom.find('.m-score').text()
-    const description =cont.find('.m-description p').text()
-    const area = cont.find('.area').text()
-    const actor =cont.find('.actor').text()
+    // const score = titleDom.find('.m-score').text()
+    // const description =cont.find('.m-description p').text()
+    // const area = cont.find('.area').text()
+    // const actor =cont.find('.actor').text()
     const dropmenu=  cont.find('.dropmenu')
     let  tabs =[]
     let tab = null  
     if(!dropmenu.text()){
-      tab =   cont.find('.ea-site').data('tab')
+      tab = cont.find('.ea-site').data('tab')
       tabs.push(tab)
     }else{
         dropmenu.find('a').each(function(){
@@ -89,20 +127,38 @@ function aa($,dom,movieList){
     movieList.push({
         // url:url,
         moviePic:moviePic,
+        type:type,
         title:title,
         playtype:playtype,
-        score:score,
-        description:description,
-        area:area,
-        actor:actor,
+        // score:score,
+        // description:description,
+        // area:area,
+        // actor:actor,
         tabs:encodeTab(tabs)
     })
     return movieList
 }
-async function ss(){
-
+async function getZongyiSeriesNumber(tab){
+    url ='https://www.360kan.com/cover/zongyilistv2?id='+tab[1]+'&site='+tab[2]+'&do=switchsite&isByTime=true'
+    const html  =  await request(url)
+    var $ = cheerio.load(JSON. parse(html).data)
+    let seriesNumber ={}
+    $('.js-link').each(function(k){
+        const href =$(this).attr('href')
+        const hint =$(this).find('.w-newfigure-hint').text()
+        const title =$(this).find('.s1').text()
+        const img =$(this).find('img').data('src')
+        item={
+            href:href,
+            title : title,
+            img : img
+        }
+        seriesNumber[hint] = item
+        
+    })
+    return seriesNumber
 }
-async function getSeriesNumber(tab){
+async function getDianshiSeriesNumber(tab){
     url ='https://www.360kan.com/cover/switchsitev2?site='+tab[2]+'&id='+tab[1]+'&category='+tab[0]
     // return
                  ////console.log(url)
