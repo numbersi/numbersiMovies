318115cf960914eea0a67a299cb26a8c5a527c7e
numbersi <136222866@qq.com>
2018-11-19 18:31:18 +0800

    '修改pm2'

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -24,7 +24,7 @@ module.exports = {
       ref  : 'origin/master',
       repo : 'git@github.com:numbersi/numbersiMovies.git',
       path : '/var/www/html/wechat/production',
-      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+      'post-deploy' : 'npm install && pm2 startOrRestart ecosystem.config.js --env production'
     },
     "NODE_ENV": "production"
   }
