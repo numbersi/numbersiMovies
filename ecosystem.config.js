ca765f234314fa161dda6cf4e34b0c1dd31af808
javaxx <136222866@qq.com>
2019-01-02 13:20:37 +0800

    重构pm2 ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,8 +1,7 @@
 module.exports = {
   apps : [{
     name: 'API',
-    script: 'bin/www',
-
+    script: 'app.js',
     // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
     args: 'one two',
     instances: 1,
